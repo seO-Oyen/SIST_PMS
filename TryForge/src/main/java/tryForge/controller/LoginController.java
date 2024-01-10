ea82448e98f457e3fa2a6d5b10604a3a47a901f0
YoonSeo <kky77209@gmail.com>
2024-01-10 11:42:27 +0900

    login controller 추가

---
+++ TryForge/src/main/java/tryForge/controller/LoginController.java
@@ -0,0 +1,15 @@
+package tryForge.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping("login.do")
+	public String login() {
+		
+		return "user\\login";
+	}
+
+}
