bfb16a406ad22976384aa3e00b82bf2b609163a3
limsol02 <yesshol@naver.com>
2024-01-11 23:29:29 +0900

    바보임솔입니다

--- TryForge/src/main/java/tryForge/admin/controller/NoticeController.java
+++ TryForge/src/main/java/tryForge/admin/controller/NoticeController.java
@@ -17,14 +17,16 @@ public class NoticeController {
 	@Autowired(required = false)
 	private NoticeService service;
 	
+	// 공지사항 관리페이지 검색, 조회기능
 	@RequestMapping("noticeList.do")
 	public String noticeList(Notice sch, Model d) {
 		d.addAttribute("noticeList", service.noticeList(sch));
 		return "notice/noticeList";
 	}
 	
+	// 관리자 메인페이지 공지사항 출력
 	@GetMapping("ad_main.do")
-	public String main(Notice sch, Model d) {
+	public String ad_main(Notice sch, Model d) {
 		d.addAttribute("noticeList", service.noticeList(sch));
 	    return "ad_main\\ad_main";
 	}
