3e2e43c4fb98b4ad0fd638036942b6214af26add
hyeon2z <148454038+hyeon2z@users.noreply.github.com>
2024-01-13 22:56:57 +0900

    대시보드 서비스컨트롤러 생성

--- TryForge/src/main/java/tryForge/dashboard/controller/DashBoardController.java
+++ TryForge/src/main/java/tryForge/dashboard/controller/DashBoardController.java
@@ -1,8 +1,12 @@
 package tryForge.dashboard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DashBoardController {
-
+    @RequestMapping("test.do")
+    public String test(){
+        return "index";
+    }
 }
---
+++ TryForge/src/main/java/tryForge/dashboard/service/DashBoardService.java
@@ -0,0 +1,8 @@
+package tryForge.dashboard.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DashBoardService {
+
+}
